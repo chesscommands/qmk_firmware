489368e58b7faea12685c0787f402bc6441f3322
asakuno <chesscommands@users.noreply.github.com>
2018-11-03 21:16:24 +0900

    数字などをずらしたが,ちょっとだめだった.

--- keyboards/ergo42/keymaps/chesscommands/keymap.c
+++ keyboards/ergo42/keymaps/chesscommands/keymap.c
@@ -308,19 +308,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Move Layer
    * ,------------------------------------------------.   ,------------------------------------------------.
-   * | RESET|Escape|      |      |      |      |CapLck|   | PgUp |PgDown| Home | End  | SysRq|Bkspc |      |
+   * | RESET|  1   |  2   |  3   |  4   |  5   |CapLck|   |  6   |  7   |  8   |  9   |  0   |Bkspc |      |
    * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |   |  Å©  |  Å´  |  Å™  |  Å®  | -_   | Enter|      |
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
-   * |      |  1   |  2   |  3   |  4   |  5   |LShift|   |  6   |  7   |  8   |  9   |  0   |  _   |      |
+   * |      |      |      |      |      |      |LShift|   | PgUp |PgDown| Home | End  | SysRq|  _   |      |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
    * |      |ScrLok|      |      |      | LCtrl| LAlt |  |ó\ñÒçœÇ›|     |  +   |  _   |  |   |  `   |      |
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [MOVESPlate] = LAYOUT(
-	RESET,	KC_ESCAPE,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	KC_CAPSLOCK,			KC_PGUP,	KC_PGDOWN,	KC_HOME,	KC_END,	KC_PSCREEN,	KC_BSPACE,	KC_NO,	
+	RESET,	KC_1,	KC_2,	KC_3,	KC_4,	KC_5,	KC_CAPSLOCK,			KC_6,	KC_7,	KC_8,	KC_9,	KC_0,	KC_BSPACE,	KC_NO,	
 	KC_NO,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,			KC_LEFT,	KC_DOWN, KC_UP,   KC_RGHT, KC_MINUS, KC_ENTER, KC_NO,	
-	KC_NO,	KC_1,	KC_2,	KC_3,	KC_4,	KC_5,	_______,			KC_6,	KC_7,	KC_8,	KC_9,	KC_0,	KC_UNDERSCORE,	KC_NO,	
+	KC_NO,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	XXXXXXX,	_______,			KC_PGUP,	KC_PGDOWN,	KC_HOME,	KC_END,	KC_PSCREEN,	KC_UNDERSCORE,	KC_NO,	
 	KC_NO,	KC_LOCKING_SCROLL,	_______,	_______,	_______,	_______,	_______,			KC_NO,	_______, KC_PLUS, KC_UNDERSCORE, KC_PIPE, KC_GRAVE, KC_NO	
   ),
 
@@ -332,19 +332,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Media and mouse keys
    * ,------------------------------------------------.   ,------------------------------------------------.
-   * | åpè≥ |  F1  |  F2  |  F3  |  F4  |  F5  | åpè≥ |   |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 | åpè≥ |
+   * | åpè≥ |  !   |  @   |  #   |  $   |  %   | åpè≥ |   |  ^   |  &   |  *   |  (   |  )   |      | åpè≥ |
    * |------|------+------+------+------+------+------+   |------+------+------+------+------+------|------+
-   * | åpè≥ |  !   |  @   |  #   |  $   |  %   | PgUp |   |  ^   |  &   |  *   |  (   |  )   |  F12 | åpè≥ |
+   * | åpè≥ |  F1  |  F2  |  F3  |  F4  |  F5  | PgUp |   |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 | åpè≥ |
    * |------|------+------+------+------+------+------+   |------+------+------+------+------+------|------+
-   * | åpè≥ |LShift| Home | End  | Del  | Bkspc| PgDn |   |      |  |   |   ,  |   .  |   /  |   ;  | åpè≥ |
+   * | åpè≥ |LShift| Home | End  | Del  | Bkspc| PgDn |   |  |   |   ,  |   .  |   /  |   ;  |  F12 | åpè≥ |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
    * |      |      |      |     |ó\ñÒçœÇ›|     |      |  |îºäp/ëSäp|RSft|      |      |      |      |      |
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [MEDIAPlate] = LAYOUT(
-	_______,	KC_F1,	KC_F2,	KC_F3,	KC_F4,	KC_F5,	_______,			KC_F6,	KC_F7,	KC_F8,	KC_F9,	KC_F10,	KC_F11,	_______,	
-	_______,	KC_EXCLAIM,	KC_AT,	KC_HASH,	KC_DOLLAR,	KC_PERCENT,	KC_PGUP,			KC_CIRCUMFLEX,	KC_AMPERSAND,	KC_ASTERISK,	KC_LEFT_PAREN,	KC_RIGHT_PAREN,	KC_F12,	_______,	
-	_______,	KC_RSHIFT,	KC_HOME,	KC_END,	KC_DELETE,	KC_BSPACE, 	KC_PGDOWN,		KC_NO,	KC_PIPE,	KC_COMM,	KC_DOT,	KC_SLSH,	KC_SCLN,	_______,	
+	_______,	KC_EXCLAIM,	KC_AT,	KC_HASH,	KC_DOLLAR,	KC_PERCENT,	_______,			KC_CIRCUMFLEX,	KC_AMPERSAND,	KC_ASTERISK,	KC_LEFT_PAREN,	KC_RIGHT_PAREN,	KC_NO,	_______,	
+	_______,	KC_F1,	KC_F2,	KC_F3,	KC_F4,	KC_F5, 	KC_PGUP,			KC_F6,	KC_F7,	KC_F8,	KC_F9,	KC_F10,	KC_F11,	_______,	
+	_______,	KC_RSHIFT,	KC_HOME,	KC_END,	KC_DELETE,	KC_BSPACE,	KC_PGDOWN,			KC_PIPE,	KC_COMM,	KC_DOT,	KC_SLSH,	KC_SCLN,	KC_F12,	_______,	
 	_______,	_______,	_______,	_______,	_______,	_______,	_______,			HANZENjap_eng,	_______,	_______,	_______,	_______,	_______,	_______	
   ),
 
